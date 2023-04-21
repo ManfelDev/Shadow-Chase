af7c21bf73eef0987ef16aceaf4a338be5a216bb
Rafael José <ra.david@hotmail.com>
2023-04-21 15:11:07 +0100

    Add more precision to the player movement
    Add mask to the ground instead of tag

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,77 +4,95 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] public float moveSpeed = 70.0f;
-    [SerializeField] public float jumpForce = 90.0f;
-    private bool isGrounded = true;
+    [SerializeField]
+    public float        moveSpeed = 70.0f;
+    [SerializeField]
+    public float        jumpForce = 90.0f;
+    [SerializeField]
+    private Transform   groundDetector;
+    [SerializeField]
+    private float       groundDetectorRadius = 2;
+    [SerializeField]
+    private float       groundDetectorExtraRadius = 6.0f;
+    [SerializeField]
+    private LayerMask   groundMask;
+    [SerializeField]
+    private Collider2D  groundCollider;
+    [SerializeField]
+    private Collider2D  airCollider;
 
-    private Rigidbody2D rb;
-    private Animator animator;
+    private             Rigidbody2D rb;
+    private             Animator animator;
 
+    private bool        onGround = false;
+    
     // Start is called before the first frame update
     void Start()
     {
-        // Get the rigidbody component
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Move the player horizontally
-        MoveHorizontally();
+        DetectGround();
+
+        groundCollider.enabled = onGround;
+        airCollider.enabled = !onGround;
+
+        // Update character horizontal velocity
+        Vector2 currentVelocity = rb.velocity;
+        float speedX = Input.GetAxis("Horizontal");
+        currentVelocity.x = speedX * moveSpeed;
 
         // Check if the player is grounded and the jump button is pressed
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        if (onGround && Input.GetButtonDown("Jump"))
         {
-            Jump();
+            // Calculate the velocity needed to achieve the desired jump height
+            currentVelocity.y = Mathf.Sqrt(2f * rb.gravityScale * jumpForce * rb.mass);
+            // Apply gravity
+            currentVelocity.y -= rb.gravityScale * Time.deltaTime;
         }
-    }
 
-    // Move the player horizontally
-    private void MoveHorizontally()
-    {
-        Vector2 currentVelocity = rb.velocity;
-        // Set the x velocity to the horizontal input times the move speed
-        currentVelocity.x = Input.GetAxis("Horizontal") * moveSpeed;
+        // Apply movement
         rb.velocity = currentVelocity;
-        animator.SetFloat("Speed", Mathf.Abs(currentVelocity.x));
 
-        // Flip the player sprite if needed
-        if (currentVelocity.x < 0) transform.rotation = Quaternion.Euler(0, 180, 0);
-        else if (currentVelocity.x > 0) transform.rotation = Quaternion.identity;
-    }
-
-    // Make the player jump
-    private void Jump()
-    {
-        Vector2 currentVelocity = rb.velocity;
-        // Calculate the velocity needed to achieve the desired jump height
-        currentVelocity.y = Mathf.Sqrt(2f * rb.gravityScale * jumpForce * rb.mass);
-        // Apply gravity
-        currentVelocity.y -= rb.gravityScale * Time.deltaTime;
-        rb.velocity = currentVelocity;
-        animator.SetBool("IsJumping", true);
-    }
-
-    // When the player lands on the ground
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Check if the player is colliding with the ground
-        if (collision.gameObject.tag == "Platforms")
+        // Flip the character if needed
+        if (speedX < 0)
         {
-            isGrounded = true;
-            animator.SetBool("IsJumping", false);
+            if (transform.right.x > 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
         }
+        else if (speedX > 0)
+        {
+            if (transform.right.x < 0)
+            {
+                transform.rotation = Quaternion.identity;
+            }
+        }
+
+        // Change visuals
+        animator.SetFloat("Speed", Mathf.Abs(speedX));
+        animator.SetBool("onGround", onGround);
     }
 
-    // When the player leaves the ground
-    private void OnCollisionExit2D(Collision2D collision)
+    void DetectGround()
     {
-        // Check if the player is colliding with the ground
-        if (collision.gameObject.tag == "Platforms")
+        Collider2D collider = Physics2D.OverlapCircle(groundDetector.position, groundDetectorRadius, groundMask);
+        if (collider != null) onGround = true;
+        else
         {
-            isGrounded = false;
+            collider = Physics2D.OverlapCircle(groundDetector.position - Vector3.right * groundDetectorExtraRadius, groundDetectorRadius, groundMask);
+            if (collider != null) onGround = true;
+            else
+            {
+                collider = Physics2D.OverlapCircle(groundDetector.position + Vector3.right * groundDetectorExtraRadius, groundDetectorRadius, groundMask);
+                if (collider != null) onGround = true;
+                else onGround = false;
+            }
         }
     }
 }
