810d47492349155346bd59ebf7b587c10fba191c
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-06-10 16:34:00 +0100

    Add visual UI for alarm when it's on

--- Assets/Scripts/Enemies/EnemyAlarm.cs
+++ Assets/Scripts/Enemies/EnemyAlarm.cs
@@ -5,10 +5,51 @@ using UnityEngine;
 public class EnemyAlarm : MonoBehaviour
 {
     [SerializeField] private bool alarm = false;
+    [SerializeField] private CanvasGroup alarmLight;
     public bool IsON { get => alarm; }
+    private float slider;
+    private bool sliderUp;
+    private bool sliderDown;
 
     public void Trigger()
     {
         alarm = true;
+    }
+
+    void Start()
+    {
+        slider = 0.0f;
+        sliderDown = false;
+        sliderUp = true;
+    }
+
+    void Update()
+    {
+        if (IsON)
+        {
+            if(slider > 1)
+            {
+                slider = 1.0f;
+                sliderDown = true;
+                sliderUp = false;
+            }
+            else if (slider < 0)
+            {
+                slider = 0.0f;
+                sliderDown = false;
+                sliderUp = true;
+            }
+
+            if (sliderUp)
+                slider += 0.008f;
+            
+            else if (sliderDown)
+                slider -= 0.008f;
+
+
+            alarmLight.alpha= slider;
+        }
+
+        else alarmLight.alpha= 0;
     }
 }
